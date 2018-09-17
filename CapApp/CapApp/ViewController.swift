65f2a388c6e6ca188d28c0bd04a4bb0e849596b4
user144758 <user144758@74-80-245-38.ic2net.net>
2018-09-17 11:48:21 -0700

    lets got

---
+++ CapApp/CapApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CapApp
+//
+//  Created by user144758 on 9/17/18.
+//  Copyright © 2018 user144758. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
